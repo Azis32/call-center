7b6be8028ce1efe52576b40da1bb251e0896d6d1
Azi abdul aziz <57107246+Azis32@users.noreply.github.com>
2023-12-08 13:32:31 +0700

    Add files via upload

---
+++ asw.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <!-- Ganti dengan domain eksternal yang digunakan -->
+    <style>
+        /* ... (Gaya CSS yang sama seperti sebelumnya) ... */
+    </style>
+    <link rel="stylesheet" href="styles.css" media="screen"> <!-- Tambahkan media attribute -->
+    <script defer src="scripts.js"></script> <!-- Gunakan defer untuk skrip non-kritis -->
+    <title>Pundikas Call Center 081366627622</title>
+</head>
+
+<body>
+    <!-- Konten HTML tetap sama seperti sebelumnya -->
+</body>
+
+</html>
---
+++ sitemap.js
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://www.pundikas.rf.gd/</loc>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://www.pundikas.rf.gd/styles.css</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://www.pundikas.rf.gd/Img/unnamed.png</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+</urlset>
